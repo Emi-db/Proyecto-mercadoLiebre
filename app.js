b069a8bf0b34cfea591c5de56221a44d4860c1a3
Emi-db <emiliano.dallabella@gmail.com>
2021-09-28 19:38:57 -0300

    agregado procfile

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require("path")
 const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath))
-app.listen(3030, () => {
-    console.log("server corriendo")
+app.listen(process.env.PORT || 3030, () => {
+    console.log("server corriendo en puerto 3030")
 })
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./Views/home.html"))
